47e551181791eea863f07f089f171ff5ba6e2391
Courtney Gulledge <courtney.gulledge@gmail.com>
2023-03-06 15:29:42 -0500

    4-update colony header based on governor selected

--- scripts/Colonies.js
+++ scripts/Colonies.js
@@ -1,16 +1,18 @@
 //cg mimicked code from Cars 'R Us
-import { getGovernors } from "./database.js";
+// need setGovernor on database.js
+import { getGovernors, getColonies, setGovernor } from "./database.js";
 const governors = getGovernors()
+const colonies = getColonies()
 
-// document.addEventListener(
-//     "change",
-//     (changeEvent) => {
-//         if (changeEvent.target.id === "governor") {
-//             const chosenOption = changeEvent.target.value
-//             setGovernor(parseInt(changeEvent.target.value))
-//         }
-//     }
-// )
+document.addEventListener(
+    "change",
+    (changeEvent) => {
+        if (changeEvent.target.id === "governor") {
+            const chosenOption = changeEvent.target.value
+            setGovernor(parseInt(changeEvent.target.value))
+        }
+    }
+)
 
 export const Governors = () => {
     let html = ""
@@ -24,4 +26,18 @@ export const Governors = () => {
     html += `</select>`
 
     return html
+}
+
+// issue #4 - update colony name (heading) based on governor selected
+export const findColony = (governor, allColonies) => {
+    let governorColony = null
+    let html = ""
+
+    for (const colony of allColonies) {
+        if (colony.id === governor.colonyId) {
+            governorColony = colony
+        }
+    }
+
+    return html += `<h2>${governorColony} Minerals</h2>`
 }
