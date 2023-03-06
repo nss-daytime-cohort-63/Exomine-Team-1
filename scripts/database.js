04b045f48af4d9642007c630add4d84541efec42
Nic Lahde <nic.lahde@gmail.com>
2023-03-06 11:51:40 -0600

    merge main

--- scripts/database.js
+++ scripts/database.js
@@ -1,14 +1,35 @@
 const database = {
+    mines: [
+        {id:1,name:"Crazy Vaclav's House of Adamantium", isActive:true},
+        {id:2,name:"Unobtainium Emporium", isActive:true},
+        {id:3,name:"You Rock Mining Corp", isActive:true},
+        {id:4,name:"Can You Dig it Mine", isActive:true},
+        {id:5,name:"Super Happy Fun Mining for Happy Workers and Not Slaves", isActive:false}
+    ],
+    mineInventory:[
+        {}
+    ],
+    minerals:[
+        {id:1,name:"vibranium"},
+        {id:2,name:"adamantium"},
+        {id:3,name:"unobtainium"},
+        {id:4,name:"mythril"},
+        {id:5,name:"kryptonite"},
+        {id:6,name:"Pym particles"}
+    ],
     transientState: {}
 }
 
-export const setFacility = (facilityId) => {
-    database.transientState.selectedFacility = facilityId
+export const setMine = (mineId) => {
+    database.transientState.selectedMine = mineId
     document.dispatchEvent( new CustomEvent("stateChanged") )
 }
 
-export const getFacilities = () => {
-    return database.facilities.map(f => ({...f}))
+export const getMines = () => {
+    return database.mines.map(mine => ({...mine}))
+}
+export const getMineral = () => {
+    return database.minerals.map(mineral => ({...mineral}))
 }
 
 export const purchaseMineral = () => {
@@ -17,4 +38,4 @@ export const purchaseMineral = () => {
         // application can re-render and update state
         document.dispatchEvent( new CustomEvent("stateChanged") )
     }
-}
+
