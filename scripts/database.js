d66acce5d02000f775d4f4a3e76b09fb6801230e
Courtney Gulledge <courtney.gulledge@gmail.com>
2023-03-06 12:27:36 -0500

    database with governors and colonies

--- scripts/database.js
+++ scripts/database.js
@@ -1,4 +1,24 @@
 const database = {
+    governors: [
+        { id: 1, name: "Nic Lahde", colonyId: 1, isActive: true },
+        { id: 2, name: "Jason Harris", colonyId: 3, isActive: true },
+        { id: 3, name: "Alex Bishop", colonyId: 2, isActive: true },
+        { id: 4, name: "Cullen Ruscin", colonyId: 1, isActive: false },
+        { id: 5, name: "Courtney Gulledge", colonyId: 2, isActive: true },
+        { id: 6, name: "Josh Barton", colonyId: 3, isActive: true },
+        { id: 7, name: "Caroline Madison", colonyId: 2, isActive: false },
+        { id: 8, name: "Jeremy Myers", colonyId: 1, isActive: false }
+    ],
+    colonies: [
+        { id: 1, name: "Europa Minerals" },
+        { id: 2, name: "Colony Minerals" },
+        { id: 3, name: "Mars Minerals" }
+    ],
+    colonyInventory: [
+        {  
+
+        }
+    ],
     transientState: {}
 }
 
