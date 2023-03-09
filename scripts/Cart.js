2f6b72c118bb707c9eabea6fb54f18665bb0d443
Nic Lahde <nic.lahde@gmail.com>
2023-03-09 10:27:22 -0600

    missing selectors in Cart restored

--- scripts/Cart.js
+++ scripts/Cart.js
@@ -1,4 +1,6 @@
 import { addCustomOrder, getColonies, getMines, getMineral, getColonyInventory, getMineInventory, getCurrentOrder } from "./database.js"
+import { renderColonyInventory } from "./Colonies.js"
+import { MineInventory } from "./MineInventory.js"
 const mines = getMines()
 const mineInventories = getMineInventory()
 
@@ -7,6 +9,7 @@ const colonies = getColonies()
 const minerals = getMineral()
 const findMineInventory = (mineId) => {
     const currentOrder = getCurrentOrder()
+    const colonyInventories = getColonyInventory()
     let currentMine = null
     let currentMineInventory = null
     for (const mine of mines) {
@@ -23,10 +26,10 @@ const findMineInventory = (mineId) => {
     }
     return currentMineInventory
 }
+
+let currentColony = null//moved to modular scope
 const findMatchingColonyInventory = (colonyId) => {
     const currentOrder = getCurrentOrder()
-    const colonyInventories = getColonyInventory()
-    let currentColony = null
     let currentColonyInventory = null
     for (const colony of colonies) {
         if (colonyId === colony.id) {
@@ -65,6 +68,7 @@ const findMatchingColonyInventory = (colonyId) => {
 //     return currentMineral
 // }
 
+let currentMineId = 0;
 export const cartUpdate = () => {
     const currentOrder = getCurrentOrder()
     let currentMineId = currentOrder.selectedMine
@@ -96,7 +100,7 @@ export const purchaseButton = () => {
                 let buyer = findMatchingColonyInventory(currentOrder.selectedColony)
                 // let product = findMineral(currentOrder.selectedMineral)
                 if(seller.quantity === 0){
-                    window.alert("Item is out of stock")
+                    window.alert("Item is out of stock")//When this line is reached, it doesn't actually stop an order from placing.
                 } 
                 else{ 
                     if (buyer === null) {
@@ -107,6 +111,8 @@ export const purchaseButton = () => {
                     seller.quantity = seller.quantity - 1
                 }
                 }
+                document.querySelector("#colony--inventory").innerHTML = renderColonyInventory(currentColony);
+                document.querySelector("#mine--inventoryDisplay").innerHTML=MineInventory(currentOrder.selectedMine);
             }
         }
     )
